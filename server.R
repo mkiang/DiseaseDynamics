b2425d8b1cf681c9373a1c62e7570996c3f20f28
Mathew Kiang <mathew.kiang@gmail.com>
2013-12-20 17:55:56 -0500

    Update server.R
    Major.

--- server.R
+++ server.R
@@ -3,9 +3,7 @@
 # errors are contained within. 
 
 ## Load packages we'll need.
-library(shiny)
-library(deSolve) 
-library(ggplot2)
+library(shiny); library(deSolve); library(ggplot2); library(quantmod)
 
 ## Define various comparmental models we will use later. All models are in an
 ## open population, but that changes by simply setting births and deaths to 0.
@@ -28,9 +26,9 @@ library(ggplot2)
 SIR.open <- function(t, x, parms) {
   with(as.list(c(parms, x)), {
     N <- S + I + R
-    dS <- birth*N - beta*S*I/N - death*S
+    dS <- birth*N - beta*S*I/N - death*S - pv*birth*N
     dI <- beta*S*I/N - r*I - death*I
-    dR <- r*I - death*R
+    dR <- r*I - death*R + pv*birth*N
     result <- c(dS, dI, dR)
     list(result)
   })
@@ -40,10 +38,10 @@ SIR.open <- function(t, x, parms) {
 SEIR.open <- function(t, x, parms)  {
   with(as.list(c(parms, x)), {
     N <- S + E + I + R
-    dS <- birth*N - beta*S*I/N - death*S
+    dS <- birth*N - beta*S*I/N - death*S - pv*birth*N
     dE <- beta*S*I/N - L*E - death*E
     dI <- L*E - r*I - death*I
-    dR <- r*I - death*R
+    dR <- r*I - death*R + pv*birth*N
     result <- c(dS, dE, dI, dR)
     list(result)
   })
@@ -55,9 +53,9 @@ SIR.kt <- function(t, x, parms) {
     k <- k0 * (1 + k1 * cos(2 * pi * (t/t.adj)))  
     beta <- b * k
     N <- S + I + R
-    dS <- -beta*S*I/N + birth*N - death*S
+    dS <- -beta*S*I/N + birth*N - death*S - pv*birth*N
     dI <- beta*S*I/N - r*I - death*I
-    dR <- r*I - death*R
+    dR <- r*I - death*R + pv*birth*N
     result <- c(dS, dI, dR)
     list(result)
   })
@@ -69,10 +67,10 @@ SEIR.kt <- function(t, x, parms)  {
     k <- k0 * (1 + k1 * cos(2 * pi * (t/t.adj)))  
     beta <- b * k
     N <- S + E + I + R
-    dS <- -beta*S*I/N + birth*N - death*S
+    dS <- -beta*S*I/N + birth*N - death*S - pv*birth*N
     dE <- beta*S*I/N - k*E - death*E
     dI <- k*E - r*I - death*I
-    dR <- r*I - death*R
+    dR <- r*I - death*R + pv*birth*N
     result <- c(dS, dE, dI, dR)
     list(result)
   })
@@ -80,10 +78,18 @@ SEIR.kt <- function(t, x, parms)  {
 
 ## Define a function to find max infections to put on plots later.
 max.inf <- function(x) {
-  infectious <- round(max(x$people[x$compartments=="I"], na.rm=TRUE))
+  infectious <- round(max(x$people[x$compartments=="I"]))
   timestep <- which.max(x$people[x$compartments=="I"])
   info <- cbind(infectious, timestep)
   return(info)
+}
+
+## Index of second highest infection value.
+sec.index <- function(x) {
+  index <- round(findPeaks(x$people[x$compartments=="I"], thresh=.1)[2])
+  if (is.na(index)) {
+    return(0)
+  } else return(as.integer(index))
 }
 
 ## Makes the data in a more ggplot-friendly format
@@ -100,20 +106,17 @@ stacker <- function(df){
 shinyServer(function(input, output) { 
   output$guessPlot <- renderPlot(function() {
     
-    # To optimize responsiveness (i.e., minimize computation), we change
-    # the scale when the number of time points gets too high. However,
-    # this means we also need to change the x-axis labels and the annotation
-    # on the graph. We will use divtime to do that. We also need to know
-    # if we are using months or days in order to adjust the rates.
     if (input$timex=="days") {
-      r.adj <- 1; k.adj <- 1/7; bd.adj <- 365; L.adj <- 1; t.adj <- 365
+      r.adj <- 1; k.adj <- 1/7; bd.adj <- 365; L.adj <- 1; t.adj <- 365; pv.adj<-1
       timemax <- input$tmaxday
     } else {
-      r.adj<-365/12; k.adj<-52/12; bd.adj<-12; L.adj<-365/12; t.adj<-12
-      timemax <- input$tmaxmonth
+      r.adj<-365; k.adj<-52; bd.adj<-1; L.adj<-365; t.adj<-1; pv.adj<-1
+      timemax <- input$tmaxyear
     }
-    if (timemax <= 180) divtime <- 100 else divtime <- 10
-    dt <- seq(0, timemax, by=1/divtime)
+    
+    dt <- seq(0, timemax, by=.01)
+    
+    if (input$norecov==TRUE) r.adj <- 0
     
     # First, making sure we use the right model.
     ## SIR without seasonal effects
@@ -122,95 +125,97 @@ shinyServer(function(input, output) {
       b <- input$b
       k <- input$k * k.adj
       birth <- death <- input$bdrate/bd.adj
+      pv <- (input$vacc*input$vacceff)/pv.adj
       
       inits <- c(S=1e6-1, I=1, R=0)
-      guess_pars <- c(r=r, beta=b*k, birth=birth, death=death)
+      guess_pars <- c(r=r, beta=b*k, birth=birth, death=death, pv=pv)
       guess <- stacker(as.data.frame(ode(inits, dt, SIR.open, 
                                          parms=guess_pars)))
     } else if (input$L!=0 & input$seasonal==0) {
-   
-    ## SEIR without seasonal effects
+      
+      ## SEIR without seasonal effects
       r <- (1/input$D) * r.adj
       b <- input$b
       k <- input$k * k.adj
       L <- (1/input$L) * L.adj
       birth <- death <- input$bdrate/bd.adj
+      pv <- (input$vacc*input$vacceff)/pv.adj
       
       inits <- c(S=1e6-1, E=0, I=1, R=0)
-      guess_pars <- c(r=r, beta=b*k, birth=birth, death=death, L=L)
+      guess_pars <- c(r=r, beta=b*k, birth=birth, death=death, L=L, pv=pv)
       guess <- stacker(as.data.frame(ode(inits, dt, SEIR.open, 
                                          parms=guess_pars)))
     } else if (input$L==0 & input$seasonal!=0)  {
-    
-    ## SIR with seasonal effects
+      
+      ## SIR with seasonal effects
       r <- (1/input$D) * r.adj
       b <- input$b
       k0 <- input$k * k.adj
       k1 <- input$seasonal
       birth <- death <- input$bdrate/bd.adj
+      pv <- (input$vacc*input$vacceff)/pv.adj
       
       inits <- c(S=1e6-1, I=1, R=0)
       guess_pars <- c(r=r, b=b, birth=birth, death=death, k0=k0, k1=k1,
-                      t.adj=t.adj)
+                      t.adj=t.adj, pv=pv)
       guess <- stacker(as.data.frame(ode(inits, dt, SIR.kt, 
                                          parms=guess_pars)))
     } else if (input$L!=0 & input$seasonal!=0)  {
       
-    ## SEIR with seasonal effects
+      ## SEIR with seasonal effects
       r <- (1/input$D) * r.adj
       b <- input$b
       k0 <- input$k * k.adj
       k1 <- input$seasonal
       L <- (1/input$L) * L.adj
       birth <- death <- input$bdrate/bd.adj
+      pv <- (input$vacc*input$vacceff)/pv.adj
       
       inits <- c(S=1e6-1, E=0, I=1, R=0)
       guess_pars <- c(r=r, b=b, birth=birth, death=death, k0=k0, k1=k1,
-                      t.adj=t.adj, L=L)
+                      t.adj=t.adj, L=L, pv=pv)
       guess <- stacker(as.data.frame(ode(inits, dt, SEIR.kt, 
                                          parms=guess_pars)))
+    }
+    
+    if (input$second.inf==TRUE) {
+      guess <- subset(guess, time > (.95 * sec.index(guess)))
     }
     
     # Max infections
     max.guess <- as.data.frame(max.inf(guess))
     max.guess$compartments <- "I"
-    max.guess$adj.timestep <- max.guess$timestep / divtime
+    max.guess$adj.timestep <- max.guess$timestep / 100
+    max.guess$maxtime <- max(guess$time)
     
     # Base plots
     if (input$inf.only==FALSE) {
-    base.plot <- ggplot(guess, aes(x=time, y=people, 
-                                   group=compartments, color=compartments)) + 
-      xlab("Time") + ylab("Number of People")
+      base.plot <- ggplot(guess, aes(x=time, y=people, 
+                                     group=compartments, color=compartments)) + 
+        xlab("Time") + ylab("Number of People")
     } else {
-    base.plot <- ggplot(guess[guess$compartments=="I",], aes(x=time, y=people, 
-                                   group=compartments, color=compartments)) + 
-      xlab("Time") + ylab("Number of Infected")
+      base.plot <- ggplot(guess[guess$compartments=="I",], aes(x=time, y=people, 
+                                                               group=compartments, color=compartments)) + 
+        xlab("Time") + ylab("Number of Infected")
     }
     label.plot <- base.plot + geom_line(alpha=.8) + 
       theme(legend.position="bottom") + theme(legend.title=element_blank())
     
-    # Conditioning plots
-    if (input$maxinfect==TRUE) {
-      rescaledx.plot <- label.plot + 
-        scale_x_continuous(labels=function(x) x/divtime) 
-    } else {
-        rescaledx.plot <- label.plot + 
-        scale_x_continuous(labels=function(x) x/divtime) + 
-        geom_text(data=max.guess, 
-                  aes(timestep*1.2, infectious, 
-                      label=paste(infectious, "infections\nat", adj.timestep)), 
-                  hjust=0, vjust=1, show_guide=FALSE, size=5, 
-                  alpha=.5, color="black")
-    }
+    #         # Conditioning plots
+    #         if (input$maxinfect==TRUE) {
+    #             
+    #         } else {
+    #             rescaledx.plot <- label.plot + 
+    #                 scale_x_continuous(labels=function(x) x/100) +
+    #                 geom_text(data=max.guess, 
+    #                           aes(x=.95*maxtime, y=infectious, 
+    #                               label=paste(infectious, "infections\nat", adj.timestep)), 
+    #                           hjust=1, vjust=1, show_guide=FALSE, size=5, 
+    #                           alpha=.5, color="black")
+    #         }
     
-    # Log(y)?
-    if (input$logy==TRUE) {
-      final.plot <- rescaledx.plot + coord_trans(y="log10")
-    } else {
-      final.plot <- rescaledx.plot
-    }
-    
-    
+    rescaledx.plot <- label.plot + scale_x_continuous(labels=function(x) x/100)
+    final.plot <- rescaledx.plot
     print(final.plot)
     
   })  # closes reactivePlot

aaed2785384715b87a94d07c81584f803e8bfa42
mkiang <mathew.kiang@gmail.com>
2015-02-13 21:28:02 -0500

    Reconciliation
    Had to reconcile two (local) copies of the code. Merged and updated.

--- server.R
+++ server.R
@@ -24,199 +24,199 @@ library(shiny); library(deSolve); library(ggplot2); library(quantmod)
 
 ### Basic SIR open population 
 SIR.open <- function(t, x, parms) {
-  with(as.list(c(parms, x)), {
-    N <- S + I + R
-    dS <- birth*N - beta*S*I/N - death*S - pv*birth*N
-    dI <- beta*S*I/N - r*I - death*I
-    dR <- r*I - death*R + pv*birth*N
-    result <- c(dS, dI, dR)
-    list(result)
-  })
+    with(as.list(c(parms, x)), {
+        N <- S + I + R
+        dS <- birth*N - beta*S*I/N - death*S - pv*birth*N
+        dI <- beta*S*I/N - r*I - death*I
+        dR <- r*I - death*R + pv*birth*N
+        result <- c(dS, dI, dR)
+        list(result)
+    })
 }
 
 ### Basic SEIR open population
 SEIR.open <- function(t, x, parms)  {
-  with(as.list(c(parms, x)), {
-    N <- S + E + I + R
-    dS <- birth*N - beta*S*I/N - death*S - pv*birth*N
-    dE <- beta*S*I/N - L*E - death*E
-    dI <- L*E - r*I - death*I
-    dR <- r*I - death*R + pv*birth*N
-    result <- c(dS, dE, dI, dR)
-    list(result)
-  })
+    with(as.list(c(parms, x)), {
+        N <- S + E + I + R
+        dS <- birth*N - beta*S*I/N - death*S - pv*birth*N
+        dE <- beta*S*I/N - L*E - death*E
+        dI <- L*E - r*I - death*I
+        dR <- r*I - death*R + pv*birth*N
+        result <- c(dS, dE, dI, dR)
+        list(result)
+    })
 }
 
 ### SEIR with seasonal variation
 SIR.kt <- function(t, x, parms) {
-  with(as.list(c(parms, x)), {
-    k <- k0 * (1 + k1 * cos(2 * pi * (t/t.adj)))  
-    beta <- b * k
-    N <- S + I + R
-    dS <- -beta*S*I/N + birth*N - death*S - pv*birth*N
-    dI <- beta*S*I/N - r*I - death*I
-    dR <- r*I - death*R + pv*birth*N
-    result <- c(dS, dI, dR)
-    list(result)
-  })
+    with(as.list(c(parms, x)), {
+        k <- k0 * (1 + k1 * cos(2 * pi * (t/t.adj)))  
+        beta <- b * k
+        N <- S + I + R
+        dS <- -beta*S*I/N + birth*N - death*S - pv*birth*N
+        dI <- beta*S*I/N - r*I - death*I
+        dR <- r*I - death*R + pv*birth*N
+        result <- c(dS, dI, dR)
+        list(result)
+    })
 }
 
 ### SEIR with seasonal variation
 SEIR.kt <- function(t, x, parms)  {
-  with(as.list(c(parms, x)), {
-    k <- k0 * (1 + k1 * cos(2 * pi * (t/t.adj)))  
-    beta <- b * k
-    N <- S + E + I + R
-    dS <- -beta*S*I/N + birth*N - death*S - pv*birth*N
-    dE <- beta*S*I/N - k*E - death*E
-    dI <- k*E - r*I - death*I
-    dR <- r*I - death*R + pv*birth*N
-    result <- c(dS, dE, dI, dR)
-    list(result)
-  })
+    with(as.list(c(parms, x)), {
+        k <- k0 * (1 + k1 * cos(2 * pi * (t/t.adj)))  
+        beta <- b * k
+        N <- S + E + I + R
+        dS <- -beta*S*I/N + birth*N - death*S - pv*birth*N
+        dE <- beta*S*I/N - k*E - death*E
+        dI <- k*E - r*I - death*I
+        dR <- r*I - death*R + pv*birth*N
+        result <- c(dS, dE, dI, dR)
+        list(result)
+    })
 }
 
 ## Define a function to find max infections to put on plots later.
 max.inf <- function(x) {
-  infectious <- round(max(x$people[x$compartments=="I"]))
-  timestep <- which.max(x$people[x$compartments=="I"])
-  info <- cbind(infectious, timestep)
-  return(info)
+    infectious <- round(max(x$people[x$compartments=="I"]))
+    timestep <- which.max(x$people[x$compartments=="I"])
+    info <- cbind(infectious, timestep)
+    return(info)
 }
 
 ## Index of second highest infection value.
 sec.index <- function(x) {
-  index <- round(findPeaks(x$people[x$compartments=="I"], thresh=.1)[2])
-  if (is.na(index)) {
-    return(0)
-  } else return(as.integer(index))
+    index <- round(findPeaks(x$people[x$compartments=="I"], thresh=.1)[2])
+    if (is.na(index)) {
+        return(0)
+    } else return(as.integer(index))
 }
 
 ## Makes the data in a more ggplot-friendly format
 stacker <- function(df){
-  df.stack <- stack(df[, -1])
-  df.stack$time <- rep(seq_len(nrow(df)), length(table(df.stack$ind)))
-  names(df.stack)[1:2] <- c("people", "compartments")
-  df.stack$compartments <- factor(df.stack$compartments, 
-                                  levels=c("S","E","I","R"), ordered=TRUE)
-  return(df.stack)
+    df.stack <- stack(df[, -1])
+    df.stack$time <- rep(seq_len(nrow(df)), length(table(df.stack$ind)))
+    names(df.stack)[1:2] <- c("people", "compartments")
+    df.stack$compartments <- factor(df.stack$compartments, 
+                                    levels=c("S","E","I","R"), ordered=TRUE)
+    return(df.stack)
 }
 
 ## Start the Shiny stuff
 shinyServer(function(input, output) { 
-  output$guessPlot <- renderPlot(function() {
-    
-    if (input$timex=="days") {
-      r.adj <- 1; k.adj <- 1/7; bd.adj <- 365; L.adj <- 1; t.adj <- 365; pv.adj<-1
-      timemax <- input$tmaxday
-    } else {
-      r.adj<-365; k.adj<-52; bd.adj<-1; L.adj<-365; t.adj<-1; pv.adj<-1
-      timemax <- input$tmaxyear
-    }
-    
-    dt <- seq(0, timemax, by=.01)
-    
-    if (input$norecov==TRUE) r.adj <- 0
-    
-    # First, making sure we use the right model.
-    ## SIR without seasonal effects
-    if(input$L==0 & input$seasonal==0)  {
-      r <- (1/input$D) * r.adj
-      b <- input$b
-      k <- input$k * k.adj
-      birth <- death <- input$bdrate/bd.adj
-      pv <- (input$vacc*input$vacceff)/pv.adj
-      
-      inits <- c(S=1e6-1, I=1, R=0)
-      guess_pars <- c(r=r, beta=b*k, birth=birth, death=death, pv=pv)
-      guess <- stacker(as.data.frame(ode(inits, dt, SIR.open, 
-                                         parms=guess_pars)))
-    } else if (input$L!=0 & input$seasonal==0) {
-      
-      ## SEIR without seasonal effects
-      r <- (1/input$D) * r.adj
-      b <- input$b
-      k <- input$k * k.adj
-      L <- (1/input$L) * L.adj
-      birth <- death <- input$bdrate/bd.adj
-      pv <- (input$vacc*input$vacceff)/pv.adj
-      
-      inits <- c(S=1e6-1, E=0, I=1, R=0)
-      guess_pars <- c(r=r, beta=b*k, birth=birth, death=death, L=L, pv=pv)
-      guess <- stacker(as.data.frame(ode(inits, dt, SEIR.open, 
-                                         parms=guess_pars)))
-    } else if (input$L==0 & input$seasonal!=0)  {
-      
-      ## SIR with seasonal effects
-      r <- (1/input$D) * r.adj
-      b <- input$b
-      k0 <- input$k * k.adj
-      k1 <- input$seasonal
-      birth <- death <- input$bdrate/bd.adj
-      pv <- (input$vacc*input$vacceff)/pv.adj
-      
-      inits <- c(S=1e6-1, I=1, R=0)
-      guess_pars <- c(r=r, b=b, birth=birth, death=death, k0=k0, k1=k1,
-                      t.adj=t.adj, pv=pv)
-      guess <- stacker(as.data.frame(ode(inits, dt, SIR.kt, 
-                                         parms=guess_pars)))
-    } else if (input$L!=0 & input$seasonal!=0)  {
-      
-      ## SEIR with seasonal effects
-      r <- (1/input$D) * r.adj
-      b <- input$b
-      k0 <- input$k * k.adj
-      k1 <- input$seasonal
-      L <- (1/input$L) * L.adj
-      birth <- death <- input$bdrate/bd.adj
-      pv <- (input$vacc*input$vacceff)/pv.adj
-      
-      inits <- c(S=1e6-1, E=0, I=1, R=0)
-      guess_pars <- c(r=r, b=b, birth=birth, death=death, k0=k0, k1=k1,
-                      t.adj=t.adj, L=L, pv=pv)
-      guess <- stacker(as.data.frame(ode(inits, dt, SEIR.kt, 
-                                         parms=guess_pars)))
-    }
-    
-    if (input$second.inf==TRUE) {
-      guess <- subset(guess, time > (.95 * sec.index(guess)))
-    }
-    
-    # Max infections
-    max.guess <- as.data.frame(max.inf(guess))
-    max.guess$compartments <- "I"
-    max.guess$adj.timestep <- max.guess$timestep / 100
-    max.guess$maxtime <- max(guess$time)
-    
-    # Base plots
-    if (input$inf.only==FALSE) {
-      base.plot <- ggplot(guess, aes(x=time, y=people, 
-                                     group=compartments, color=compartments)) + 
-        xlab("Time") + ylab("Number of People")
-    } else {
-      base.plot <- ggplot(guess[guess$compartments=="I",], aes(x=time, y=people, 
-                                                               group=compartments, color=compartments)) + 
-        xlab("Time") + ylab("Number of Infected")
-    }
-    label.plot <- base.plot + geom_line(alpha=.8) + 
-      theme(legend.position="bottom") + theme(legend.title=element_blank())
-    
-    #         # Conditioning plots
-    #         if (input$maxinfect==TRUE) {
-    #             
-    #         } else {
-    #             rescaledx.plot <- label.plot + 
-    #                 scale_x_continuous(labels=function(x) x/100) +
-    #                 geom_text(data=max.guess, 
-    #                           aes(x=.95*maxtime, y=infectious, 
-    #                               label=paste(infectious, "infections\nat", adj.timestep)), 
-    #                           hjust=1, vjust=1, show_guide=FALSE, size=5, 
-    #                           alpha=.5, color="black")
-    #         }
-    
-    rescaledx.plot <- label.plot + scale_x_continuous(labels=function(x) x/100)
-    final.plot <- rescaledx.plot
-    print(final.plot)
-    
-  })  # closes reactivePlot
+    output$guessPlot <- renderPlot(function() {
+        
+        if (input$timex=="days") {
+            r.adj <- 1; k.adj <- 1/7; bd.adj <- 365; L.adj <- 1; t.adj <- 365; pv.adj<-1
+            timemax <- input$tmaxday
+        } else {
+            r.adj<-365; k.adj<-52; bd.adj<-1; L.adj<-365; t.adj<-1; pv.adj<-1
+            timemax <- input$tmaxyear
+        }
+        
+        dt <- seq(0, timemax, by=.01)
+        
+        if (input$norecov==TRUE) r.adj <- 0
+        
+        # First, making sure we use the right model.
+        ## SIR without seasonal effects
+        if(input$L==0 & input$seasonal==0)  {
+            r <- (1/input$D) * r.adj
+            b <- input$b
+            k <- input$k * k.adj
+            birth <- death <- input$bdrate/bd.adj
+            pv <- (input$vacc*input$vacceff)/pv.adj
+            
+            inits <- c(S=1e6-1, I=1, R=0)
+            guess_pars <- c(r=r, beta=b*k, birth=birth, death=death, pv=pv)
+            guess <- stacker(as.data.frame(ode(inits, dt, SIR.open, 
+                                               parms=guess_pars)))
+        } else if (input$L!=0 & input$seasonal==0) {
+            
+            ## SEIR without seasonal effects
+            r <- (1/input$D) * r.adj
+            b <- input$b
+            k <- input$k * k.adj
+            L <- (1/input$L) * L.adj
+            birth <- death <- input$bdrate/bd.adj
+            pv <- (input$vacc*input$vacceff)/pv.adj
+            
+            inits <- c(S=1e6-1, E=0, I=1, R=0)
+            guess_pars <- c(r=r, beta=b*k, birth=birth, death=death, L=L, pv=pv)
+            guess <- stacker(as.data.frame(ode(inits, dt, SEIR.open, 
+                                               parms=guess_pars)))
+        } else if (input$L==0 & input$seasonal!=0)  {
+            
+            ## SIR with seasonal effects
+            r <- (1/input$D) * r.adj
+            b <- input$b
+            k0 <- input$k * k.adj
+            k1 <- input$seasonal
+            birth <- death <- input$bdrate/bd.adj
+            pv <- (input$vacc*input$vacceff)/pv.adj
+            
+            inits <- c(S=1e6-1, I=1, R=0)
+            guess_pars <- c(r=r, b=b, birth=birth, death=death, k0=k0, k1=k1,
+                            t.adj=t.adj, pv=pv)
+            guess <- stacker(as.data.frame(ode(inits, dt, SIR.kt, 
+                                               parms=guess_pars)))
+        } else if (input$L!=0 & input$seasonal!=0)  {
+            
+            ## SEIR with seasonal effects
+            r <- (1/input$D) * r.adj
+            b <- input$b
+            k0 <- input$k * k.adj
+            k1 <- input$seasonal
+            L <- (1/input$L) * L.adj
+            birth <- death <- input$bdrate/bd.adj
+            pv <- (input$vacc*input$vacceff)/pv.adj
+            
+            inits <- c(S=1e6-1, E=0, I=1, R=0)
+            guess_pars <- c(r=r, b=b, birth=birth, death=death, k0=k0, k1=k1,
+                            t.adj=t.adj, L=L, pv=pv)
+            guess <- stacker(as.data.frame(ode(inits, dt, SEIR.kt, 
+                                               parms=guess_pars)))
+        }
+        
+        if (input$second.inf==TRUE) {
+            guess <- subset(guess, time > (.95 * sec.index(guess)))
+        }
+        
+        # Max infections
+        max.guess <- as.data.frame(max.inf(guess))
+        max.guess$compartments <- "I"
+        max.guess$adj.timestep <- max.guess$timestep / 100
+        max.guess$maxtime <- max(guess$time)
+        
+        # Base plots
+        if (input$inf.only==FALSE) {
+            base.plot <- ggplot(guess, aes(x=time, y=people, 
+                                           group=compartments, color=compartments)) + 
+                xlab("Time") + ylab("Number of People")
+        } else {
+            base.plot <- ggplot(guess[guess$compartments=="I",], aes(x=time, y=people, 
+                                                                     group=compartments, color=compartments)) + 
+                xlab("Time") + ylab("Number of Infected")
+        }
+        label.plot <- base.plot + geom_line(alpha=.8) + theme_bw() + 
+            theme(legend.position="bottom") + theme(legend.title=element_blank())
+        
+        #         # Conditioning plots
+        #         if (input$maxinfect==TRUE) {
+        #             
+        #         } else {
+        #             rescaledx.plot <- label.plot + 
+        #                 scale_x_continuous(labels=function(x) x/100) +
+        #                 geom_text(data=max.guess, 
+        #                           aes(x=.95*maxtime, y=infectious, 
+        #                               label=paste(infectious, "infections\nat", adj.timestep)), 
+        #                           hjust=1, vjust=1, show_guide=FALSE, size=5, 
+        #                           alpha=.5, color="black")
+        #         }
+        
+        rescaledx.plot <- label.plot + scale_x_continuous(labels=function(x) x/100)
+        final.plot <- rescaledx.plot
+        print(final.plot)
+        
+    })  # closes reactivePlot
 })  # Closes shinyServer
--- ui.R
+++ ui.R
@@ -2,68 +2,69 @@ library(shiny)
 
 # Define UI
 shinyUI(pageWithSidebar(
-  headerPanel("Basic ODE Models of Disease Dynamics"),
-  sidebarPanel(
+    headerPanel("Basic ODE Models of Disease Dynamics"),
+    sidebarPanel(
+        
+        wellPanel(tags$b("Basics:"),
+                  sliderInput("b", "Probability of transmission:", 
+                              value=.75, min=0, max=1, step=.01),
+                  sliderInput("k", "Average contacts (per week):", 
+                              value=12, min=1, max=100, step=1)),
+        
+        wellPanel(tags$b("Disease properties:"),
+                  checkboxInput("norecov", "No recovery", value=FALSE),
+                  conditionalPanel(condition="input.norecov == false", 
+                                   sliderInput("D", "Duration (days):", 
+                                               value=7, min=1, max=60, step=1),
+                                   sliderInput("L", label="Latent period (days):", 
+                                               value=0, min=0, max=60, step=1),
+                                   sliderInput("seasonal", label="Seasonal fluctuations:",
+                                               min=0, max=.4, step=.005, value=0))),
+        
+        wellPanel(tags$b("Vital dynamics and vaccination:"),
+                  sliderInput("bdrate", label="Birth and death rate:",
+                              min=0, max=.04, step=.001, value=0),
+                  conditionalPanel(
+                      condition="input.bdrate>0",
+                      sliderInput("vacc", label="Proportion vaccinated at birth:",
+                                  min=0, max=1, step=.01, value=0),
+                      conditionalPanel(
+                          condition="input.vacc>0",
+                          sliderInput("vacceff", label="Vaccine effectiveness:",
+                                      min=0, max=1, step=.01, value=1)))),    
+        
+        wellPanel(tags$b("Time scale:"),
+                  selectInput("timex", label="", 
+                              choices=list("Days"="days", "Years"="years")),
+                  conditionalPanel(condition="input.timex == 'days'", 
+                                   sliderInput("tmaxday", "Time max:", 
+                                               value=65, min=5, max=180, step=5)),
+                  conditionalPanel(condition="input.timex == 'years'", 
+                                   sliderInput("tmaxyear", "Time max:", 
+                                               value=50, min=1, max=100, step=.5))),
+        
+        wellPanel(tags$b("Plot settings:"),
+                  #                   checkboxInput("maxinfect", "Hide infection info", value=FALSE),
+                  checkboxInput("inf.only", "Show infectious curve only", value=FALSE),
+                  conditionalPanel(
+                      condition="input.bdrate>0",
+                      checkboxInput("second.inf", "Post-initial outbreak only", value=FALSE))),
+        
+        wellPanel(tags$h5("Created by Mathew Kiang"), 
+                  tags$body("(", tags$a("Git", 
+                                        href="http://github.com/mkiang")," | ", 
+                            tags$a("Tweet", 
+                                   href="http://twitter.com/mattkiang")," | ",
+                            tags$a("Read", 
+                                   href="http://mathewkiang.com"),")"))),
     
-    wellPanel(tags$b("Basics:"),
-              sliderInput("b", "Probability of transmission:", 
-                          value=.75, min=0, max=1, step=.01),
-              sliderInput("k", "Average contacts (per week):", 
-                          value=12, min=1, max=100, step=1)),
-    
-    wellPanel(tags$b("Disease properties:"),
-              checkboxInput("norecov", "No recovery", value=FALSE),
-              conditionalPanel(condition="input.norecov == false", 
-                               sliderInput("D", "Duration (days):", 
-                                           value=7, min=1, max=60, step=1),
-                               sliderInput("L", label="Latent period (days):", 
-                                           value=0, min=0, max=60, step=1),
-                               sliderInput("seasonal", label="Seasonal fluctuations:",
-                                           min=0, max=.4, step=.005, value=0))),
-    
-    wellPanel(tags$b("Vital dynamics and vaccination:"),
-              sliderInput("bdrate", label="Birth and death rate:",
-                          min=0, max=.04, step=.001, value=0),
-              conditionalPanel(
-                condition="input.bdrate>0",
-                sliderInput("vacc", label="Proportion vaccinated at birth:",
-                            min=0, max=1, step=.01, value=0),
-                conditionalPanel(
-                  condition="input.vacc>0",
-                  sliderInput("vacceff", label="Vaccine effectiveness:",
-                              min=0, max=1, step=.01, value=1)))),    
-    
-    wellPanel(tags$b("Time scale:"),
-              selectInput("timex", label="", 
-                          choices=list("Days"="days", "Years"="years")),
-              conditionalPanel(condition="input.timex == 'days'", 
-                               sliderInput("tmaxday", "Time max:", 
-                                           value=65, min=5, max=180, step=5)),
-              conditionalPanel(condition="input.timex == 'years'", 
-                               sliderInput("tmaxyear", "Time max:", 
-                                           value=50, min=1, max=100, step=.5))),
-    
-    wellPanel(tags$b("Plot settings:"),
-              #                   checkboxInput("maxinfect", "Hide infection info", value=FALSE),
-              checkboxInput("inf.only", "Show infectious curve only", value=FALSE),
-              conditionalPanel(
-                condition="input.bdrate>0",
-                checkboxInput("second.inf", "Post-initial outbreak only", value=FALSE))),
-    
-    wellPanel(tags$h5("Created by Mathew Kiang"), 
-              tags$body("(", tags$a("Git", 
-                                    href="http://github.com/mkiang")," | ", 
-                        tags$a("Tweet", 
-                               href="http://twitter.com/mattkiang")," | ",
-                        tags$a("Read", 
-                               href="http://mathewkiang.com"),")"))),
-  
-  mainPanel(plotOutput("guessPlot"),
-            wellPanel(
-              tags$body(h3("About"), 
-                        p(("This is just a tool to easily visualize some basic ODE models—specifically, SI, SIR, and SIER models. You can add vital dynamics (however, birth and death rates are equal to maintain a constant population) and vaccination programs (incorportaing vaccine effectiveness). It's certainly not a comprehensive set of all math models of disease. I'm hoping to find time to code an analogous (stochastic) network-based disease dynamics page."), 
-                          h3("Things to note"), 
-                          p("Not all permutations of every parameter will render a plot (or a sensible plot). This is most often a function of a time scale that doesn't make sense given the parameters (e.g., taking a basic SIR model out to 100 years), so try adjusting your time scale first. I don't perform any computational adjustments to the time scale so increasing your time frame will result in (potentially) exponentially increased waiting times. The model always assumes a constant population of 1,000,000 people and always starts with a single infected person in an otherwise naive population. Lastly, vaccinations only occur at birth."),
-                          p("The code could certainly be extended to incorporate more complex features (e.g., disease vectors, age structures, etc.), and I encourage you to use my code on Github to do so."))
-              ))
-  )))
+    mainPanel(plotOutput("guessPlot"),
+              wellPanel(
+                  tags$body(h3("About"), 
+                            p(("This is just a tool to easily visualize some basic ODE models—specifically, SI, SIR, and SIER models. You can add vital dynamics (however, birth and death rates are equal to maintain a constant population) and vaccination programs (incorportaing vaccine effectiveness). It's certainly not a comprehensive set of all math models of disease. I'm hoping to find time to code an analogous (stochastic) network-based disease dynamics page."), 
+                              h3("Things to note"), 
+                              p("Not all permutations of every parameter will render a plot (or a sensible plot). This is most often a function of a time scale that doesn't make sense given the parameters (e.g., taking a basic SIR model out to 100 years), so try adjusting your time scale first. I don't perform any computational adjustments to the time scale so increasing your time frame will result in (potentially) exponentially increased waiting times. The model always assumes a constant population of 1,000,000 people and always starts with a single infected person in an otherwise naive population. Lastly, vaccinations only occur at birth."),
+                              p("The code could certainly be extended to incorporate more complex features (e.g., disease vectors, age structures, etc.), and I encourage you to use my code on Github to do so."), 
+                              p("For more information about the parameters, see", tags$a("this blog post.", href="http://mathewkiang.com/2013/12/20/shiny-desolve-interactive-ode-models/")))
+                  ))
+    )))
